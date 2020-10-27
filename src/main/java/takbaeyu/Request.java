a79366f7e70247dbe62c8af06aefb90de0f5556a
goforit29 <goforit29@gmail.com>
2020-10-27 17:56:37 +0900

    first commit

---
+++ src/main/java/takbaeyu/Request.java
@@ -0,0 +1,80 @@
+package takbaeyu;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Request_table")
+public class Request {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private Long memberId;
+    private Long qty;
+    private String status="Registered";
+
+    @PostPersist
+    public void onPostPersist(){
+        Requested requested = new Requested();
+        BeanUtils.copyProperties(this, requested);
+        requested.publishAfterCommit();
+
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+
+        takbaeyu.external.Payment payment = new takbaeyu.external.Payment();
+        // mappings goes here
+        payment.setRequestId(this.getId());
+        payment.setMemberId(this.getMemberId());
+        payment.setStatus("Paid");
+        RequestApplication.applicationContext.getBean(takbaeyu.external.PaymentService.class)
+            .dopay(payment);
+
+
+    }
+
+    @PreUpdate
+    public void onPreUpdate(){
+        ReqCanceled reqCanceled = new ReqCanceled();
+        BeanUtils.copyProperties(this, reqCanceled);
+        reqCanceled.publishAfterCommit();
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+    public Long getQty() {
+        return qty;
+    }
+
+    public void setQty(Long qty) {
+        this.qty = qty;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+
+}
---
+++ src/main/java/takbaeyu/external/PaymentService.java
@@ -0,0 +1,19 @@
+
+package takbaeyu.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@FeignClient(name="payment", url="${api.url.payment}")
+//@FeignClient(name="payment", url="${configurl}")
+//@FeignClient(name="payment", url="http://payment:8083")
+public interface PaymentService {
+
+    @RequestMapping(method= RequestMethod.POST, path="/payments")
+    public void dopay(@RequestBody Payment payment);
+
+}
